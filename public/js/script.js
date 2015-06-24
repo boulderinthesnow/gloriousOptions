c3285a8ce0b3cda8d99ed21d78f8b01c367e2982
Matthew Williams <mwilliamsnov6@gmail.com>
2015-06-24 15:59:23 -0700

    allR array iterating correctly

--- public/js/script.js
+++ public/js/script.js
@@ -5,42 +5,60 @@ $(function() {
     zoom: 15,
     center: myLatLong  
   }
-  function initialize() {
+  // function initialize() {
    
     map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
-    var marker = new google.maps.Marker({
-        position: myLatLong,
-        map: map,
-        title: 'Hello World!'
-    });
-    google.maps.event.addListener(window, 'load', addMarker)
-  } // end initialize
+    
+    // var marker = new google.maps.Marker({
+    //     position: myLatLong,
+    //     map: map,
+    //     title: 'Hello World!'
+    // });
 
-  google.maps.event.addDomListener(window, 'load', initialize);
 
-  function addMarker(event) {
-  	console.log(event.latLng.A);
-  	console.log(event.latLng.F);
+   // google.maps.event.addListener(window, 'load', addMarker)
+  //} // end initialize
+
+ // google.maps.event.addDomListener(window, 'load', initialize);
+
+  // function addMarker(event) {
+  // 	console.log(event.latLng.A);
+  // 	console.log(event.latLng.F);
 
   	//Add your code to add markers here
-  }
+  //}
 
 
   
   var allR = $("#allR").val();
   allR = allR.replace(/[, ]+/g, " ").trim().split("!");
-  var firstAddress = allR[0].replace(/[ ]+/g, "+");
-
-$.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + firstAddress).done(function (data) {
-   console.log(data)
-   var lat = (data.results[0].geometry.location.lat);
-   var long = (data.results[0].geometry.location.lng);
-   var address = data.results[0].formatted_address
-   console.log(lat, long, address)
-   var myLatlng = new google.maps.LatLng(lat,long);
-});
+  console.log(allR,"*********ALLR**********");
 
 
+  for (var i = 0 ; i < allR.length -1 ; i++) {
+      var urlAddress = allR[i].replace(/[ ]+/g, "+"); //add pluses for address
+      $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + urlAddress).done(function (data) {
+         console.log(data,"*********DATA**********");
+         
+         var lat = (data.results[0].geometry.location.lat);
+         var long = (data.results[0].geometry.location.lng);
+         var address = data.results[0].formatted_address
+         console.log(lat, long, address)
+         var myLatlng = new google.maps.LatLng(lat,long);
+         new google.maps.Marker({
+             position: myLatlng,
+             map: map
+         });
+      });
 
-  initialize();
-});
+  };
+
+  
+
+
+
+
+
+  // initialize();
+ });
+
