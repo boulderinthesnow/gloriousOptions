bffa7b2354f15c8d142ef98293a559da9a22a5ba
Matthew Williams <mwilliamsnov6@gmail.com>
2015-06-24 14:38:36 -0700

    my options

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,19 @@
 $(function() {
   var map;
+  var myLatLong = new google.maps.LatLng(37.7854224, -122.403726)
+  var myOptions = {
+    zoom: 15,
+    center: myLatLong  
+  }
   function initialize() {
-    var myLatLong = new google.maps.LatLng(37.7854224, -122.403726)
-    map = new google.maps.Map(document.getElementById('map-canvas'), {
-      zoom: 15,
-      center: myLatLong
-  });
+   
+    map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
     var marker = new google.maps.Marker({
         position: myLatLong,
         map: map,
         title: 'Hello World!'
     });
+    google.maps.event.addListener(window, 'load', addMarker)
   } // end initialize
 
   google.maps.event.addDomListener(window, 'load', initialize);
@@ -30,9 +33,14 @@ $(function() {
 
 $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + firstAddress).done(function (data) {
    console.log(data)
+   var lat = (data.results[0].geometry.location.lat);
+   var long = (data.results[0].geometry.location.lng);
+   var address = data.results[0].formatted_address
+   console.log(lat, long, address)
+   var myLatlng = new google.maps.LatLng(lat,long);
 });
 
 
-  console.log(firstAddress)
+
   initialize();
 });
