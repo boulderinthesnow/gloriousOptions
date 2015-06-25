0c8a3a058d50c5d1a9c8bbbd712f213a6ac30f63
Matthew Williams <mwilliamsnov6@gmail.com>
2015-06-25 16:04:40 -0700

    checkboxes added

--- public/js/script.js
+++ public/js/script.js
@@ -5,42 +5,47 @@ $(function() {
   var myOptions = {
     zoom: 13,
     center: myLatLong  
-  }
-   
-    map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
-    
+}
 
-  
-  var allR = $("#allR").val();
-  
-  allR = allR.replace(/[ ]+/g, " ").trim().split("!");
+map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
+
+
+
+var allR = $("#allR").val();
+
+allR = allR.replace(/[ ]+/g, " ").trim().split("!");
   // var allRStrip = allR[i]
   // console.log(allRStrip,"*********ALLRSTRIP**********");
 
 
 
   for (var i = 0 ; i < allR.length -1 ; i++) {
-      var urlAddress = allR[i].replace(/[ ]+/g, "+").replace(/^,/, ''); //add pluses for address
-      var urlAddressClear = urlAddress.replace(/,\+/g, ",") //should replace ,+ with +
+      var urlAddress = allR[i].replace(/[ ]+/g, "+").replace(/^,/, ''); //add pluses for address, remove starting ,
+      var urlAddressClear = urlAddress.replace(/,\+/g, ",") // replace ,+ with +
 
 
       
 
       $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + urlAddress).done(function (data) {
-         
-         var lat = (data.results[0].geometry.location.lat);
-         var long = (data.results[0].geometry.location.lng);
-         var address = data.results[0].formatted_address
-         console.log(lat, long, address)
-         var myLatlng = new google.maps.LatLng(lat,long);
 
-         new google.maps.Marker({
-             position: myLatlng,
-             map: map
-         });
-      });
+       var lat = (data.results[0].geometry.location.lat);
+       var long = (data.results[0].geometry.location.lng);
+       var address = data.results[0].formatted_address
+       console.log(lat, long, address)
+       var myLatlng = new google.maps.LatLng(lat,long);
+
+       new google.maps.Marker({
+           position: myLatlng,
+           map: map
+       });
+   });
 
   };
+
+ gf = $("#GF").click(function(){
+    console.log(gf.val())
+ })
+ console.log(gf,"*********GF**********");
 
   
 
@@ -49,5 +54,5 @@ $(function() {
 
 
   // initialize();
- });
+});
 
