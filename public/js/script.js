3f8926979c1c34e73edafdbf31a2ec27db3df744
Matthew Williams <mwilliamsnov6@gmail.com>
2015-06-24 17:20:51 -0700

    buttons to new user

--- public/js/script.js
+++ public/js/script.js
@@ -6,55 +6,9 @@ $(function() {
     zoom: 15,
     center: myLatLong  
   }
-  //function initialize() {
    
     map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
     
-    // var marker = new google.maps.Marker({
-    //     position: myLatLong,
-    //     map: map,
-    //     title: 'Hello World!'
-    // });
-
-
-   // google.maps.event.addListener(window, 'load', addMarker)
-  //} // end initialize
-
- //google.maps.event.addDomListener(window, 'load', initialize);
-
-  // function addMarker(event) {
-  // 	console.log(event.latLng.A);
-  // 	console.log(event.latLng.F);
-
-  	//Add your code to add markers here
-  //}
-
-
-
-  // function initialize() {
-  //   var myLatLong = new google.maps.LatLng(37.7854224, -122.403726)
-  //   map = new google.maps.Map(document.getElementById('map-canvas'), {
-  //     zoom: 15,
-  //     center: myLatLong
-  //   });
-
-  // } // end initialize
-
-
-
-  // google.maps.event.addDomListener(window, 'load', initialize);
-
-  // function addMarker(event) {
-  // 	console.log(event.latLng.A);
-  // 	console.log(event.latLng.F);
-
-  // 	//Add your code to add markers here
-    // var marker = new google.maps.Marker({
-    //     position: myLatLong,
-    //     map: map,
-    //     title: 'Hello World!'
-    // });
-
 
   
   var allR = $("#allR").val();
