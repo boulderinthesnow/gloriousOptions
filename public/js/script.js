bc79b47eaa4fd2f1dfaf3e6513b2b490c46f424c
Matthew Williams <mwilliamsnov6@gmail.com>
2015-06-26 16:44:29 -0700

    loadoptions working

--- public/js/script.js
+++ public/js/script.js
@@ -12,49 +12,71 @@ $(function() {
 
 
     var allR = $("#allR").val();
-    allR = allR.replace(/[ ]+/g, " ").trim().split("!");
+    console.log(allR,"*********ALLR**********");
 
- function populateFromAPI (){
+    
+    function populateFromAPI (){
+        allR = allR.replace(/[ ]+/g, " ").trim().split("!");
         for (var i = 0 ; i < allR.length -1 ; i++) {
           var urlAddress = allR[i].replace(/[ ]+/g, "+").replace(/^,/, ''); //add pluses for address, remove starting ,
           var urlAddressClear = urlAddress.replace(/,\+/g, ",") // replace ,+ with +
 
-          $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + urlAddress).done(function (data) {
-             var lat = (data.results[0].geometry.location.lat);
-             var long = (data.results[0].geometry.location.lng);
-             var address = data.results[0].formatted_address
-             console.log(lat, long, address)
-             var myLatlng = new google.maps.LatLng(lat,long);
+      $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + urlAddress).done(function (data) {
+       var lat = (data.results[0].geometry.location.lat);
+       var long = (data.results[0].geometry.location.lng);
+       var address = data.results[0].formatted_address
+       console.log(lat, long, address)
+       var myLatlng = new google.maps.LatLng(lat,long);
 
-             new google.maps.Marker({
-                 position: myLatlng,
-                 map: map
-             }); //end marker
-         });
+        new google.maps.Marker({
+               position: myLatlng,
+               map: map
+        }); //end marker
+       }); //end function
       }; // end if
   } //end populateFromAPI
 
-  function loadOptions (GF){
-    $.getJSON("/restaurants/database").done( function (restaurants) {
-        tempArr = [];
-        restaurants.forEach(function (restaurant) {
-            //console.log (restaurant.name)
-            if (restaurant.gf) {
-                tempArr.push (restaurant.address)
-            }
-        })
-        console.log(tempArr)
+  function pointsOnMap (arrayOfAddresses) {
+    for (var i = 0; i < arrayOfAddresses.length; i++) {
+        var urlAddress = arrayOfAddresses[i]
+        $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + urlAddress).done(function (data) {
+              var lat = (data.results[0].geometry.location.lat);
+              var long = (data.results[0].geometry.location.lng);
+              var address = data.results[0].formatted_address
+              console.log(lat, long, address)
+        });
+    }
+  }
+
+  function loadOptions (option){
+    var tempArr = [];
+        $.getJSON("/restaurants/database").done( function (restaurants) {
+        
+        // if (option === "GF") {
+            restaurants.forEach(function (restaurant) {
+                if (restaurant[option]) {
+                    tempArr.push(restaurant.address)
+                }
+            })
+
+        // console.log(tempArr, "TEMPARR")
+        pointsOnMap (tempArr)
+    }).fail(function(err){
+        console.log("SOMETHING WENT WRONG!", err.responseText);
     })
   }
 
   var flipper = false
+
  $("#GF").click(function (){
+    console.log("foo")
     if (flipper === false) {
         // add map points
         //populateFromAPI()
-        loadOptions()
+        var gfTemp = loadOptions("gf")
+        // console.log (loadOptions("gf"), "YTIUEHOEOBEIEJBKJEBHIJB")
+        
         // ajax call to path of my server which will query all the restrauts w/ gluten free in it
-
 
         flipper = true
         return console.log("flipper is true")
@@ -64,7 +86,6 @@ $(function() {
         flipper = false
         return console.log("flipper is false")
     }
-  
  })
 
  // $("#DF").click(function(){
