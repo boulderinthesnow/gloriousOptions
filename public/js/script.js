114038292929992243b288db09f99a1e44161d11
Matthew Williams <mwilliamsnov6@gmail.com>
2015-06-26 11:06:52 -0700

    t/f flipper

--- public/js/script.js
+++ public/js/script.js
@@ -7,60 +7,60 @@ $(function() {
     center: myLatLong  
 }
 
-map = new google.maps.Map($('#map-canvas')[0], myOptions);
+    map = new google.maps.Map($('#map-canvas')[0], myOptions);
 
 
 
-var allR = $("#allR").val();
-
-allR = allR.replace(/[ ]+/g, " ").trim().split("!");
-  // var allRStrip = allR[i]
-  // console.log(allRStrip,"*********ALLRSTRIP**********");
-
-
-
-  for (var i = 0 ; i < allR.length -1 ; i++) {
+    var allR = $("#allR").val();
+    allR = allR.replace(/[ ]+/g, " ").trim().split("!");
+    for (var i = 0 ; i < allR.length -1 ; i++) {
       var urlAddress = allR[i].replace(/[ ]+/g, "+").replace(/^,/, ''); //add pluses for address, remove starting ,
       var urlAddressClear = urlAddress.replace(/,\+/g, ",") // replace ,+ with +
 
-
-      
-
       $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + urlAddress).done(function (data) {
+         var lat = (data.results[0].geometry.location.lat);
+         var long = (data.results[0].geometry.location.lng);
+         var address = data.results[0].formatted_address
+         console.log(lat, long, address)
+         var myLatlng = new google.maps.LatLng(lat,long);
 
-       var lat = (data.results[0].geometry.location.lat);
-       var long = (data.results[0].geometry.location.lng);
-       var address = data.results[0].formatted_address
-       console.log(lat, long, address)
-       var myLatlng = new google.maps.LatLng(lat,long);
-
-       new google.maps.Marker({
-           position: myLatlng,
-           map: map
-       });
-   });
+         new google.maps.Marker({
+             position: myLatlng,
+             map: map
+         });
+     });
 
   };
 
- $("#GF").click(function(){
-    isChecked = $("#GF").is(':checked')
-    console.log("GF" + isChecked)
+  var flipper = false
+ $("#GF").click(function (){
+    if (flipper === false) {
+        // add map points
+        flipper = true
+        return console.log("flipper is true")
+    }
+    if (flipper === true) {
+        // remove map points
+        flipper = false
+        return console.log("flipper is false")
+    }
+  
  })
 
- $("#DF").click(function(){
-    isChecked = $("#DF").is(':checked')
-    console.log(isChecked)
- })
+ // $("#DF").click(function(){
+ //    isChecked = $("#DF").is(':checked')
+ //    console.log(isChecked)
+ // })
 
- $("#EF").click(function(){
-    isChecked = $("#EF").is(':checked')
-    console.log(isChecked)
- })
+ // $("#EF").click(function(){
+ //    isChecked = $("#EF").is(':checked')
+ //    console.log(isChecked)
+ // })
 
- $("#SF").click(function(){
-    isChecked = $("#SF").is(':checked')
-    console.log(isChecked)
- })
+ // $("#SF").click(function(){
+ //    isChecked = $("#SF").is(':checked')
+ //    console.log(isChecked)
+ // })
   
 
 
